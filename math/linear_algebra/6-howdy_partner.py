9d3bd9aac11ee728adc6156701055a890795d51a
yaskho <yassineholberton@gmail.com>
2025-12-07 14:54:36 +0100

    gvbnb

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module to concatenate two arrays."""
 
+
 def cat_arrays(arr1, arr2):
     """Returns a new list by concatenating arr1 and arr2."""
     return arr1 + arr2
