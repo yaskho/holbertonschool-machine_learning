6eba8e22f96c2d76078caf77a12eae0786bffa9c
yaskho <yassineholberton@gmail.com>
2025-12-07 15:41:20 +0100

    gvvhb

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
-"""
-Function to add two 2D matrices element-wise
-"""
+"""Module to add two 2D matrices element-wise."""
 
 
 def add_matrices2D(mat1, mat2):
+    """Adds two 2D matrices element-wise.
+
+    Returns a new matrix or None if shapes do not match.
     """
-    Adds two 2D matrices element-wise
-    Returns a new matrix or None if shapes mismatch
-    """
-    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+    if len(mat1) != len(mat2) or any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
         return None
-
-    new_matrix = []
-    for i in range(len(mat1)):
-        row = []
-        for j in range(len(mat1[0])):
-            row.append(mat1[i][j] + mat2[i][j])
-        new_matrix.append(row)
-
-    return new_matrix
+    return [[c1 + c2 for c1, c2 in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
