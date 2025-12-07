1c1a35ce03795f15c632645b21312a2fc03dba7c
yaskho <yassineholberton@gmail.com>
2025-12-07 15:38:12 +0100

    hhgjn

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module to perform matrix multiplication on numpy arrays."""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication between two numpy arrays."""
+    return np.matmul(mat1, mat2)
