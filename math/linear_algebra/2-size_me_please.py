61a4e5e46e732cdff7a029425dd3353588aee23c
yaskho <yassineholberton@gmail.com>
2025-12-07 14:32:01 +0100

    ggbg

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""
-Function that returns the shape of a matrix
-"""
+
+
 def matrix_shape(matrix):
+    """Returns the shape of a matrix as a list of integers."""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
-        matrix = matrix[0] if matrix else []
+        matrix = matrix[0]
     return shape
