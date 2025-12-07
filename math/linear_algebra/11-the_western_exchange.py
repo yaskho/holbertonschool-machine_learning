155de2da895cc39b1c33792afe1a207e8e5b6d1b
yaskho <yassineholberton@gmail.com>
2025-12-07 15:31:59 +0100

    hjbjnn

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module to return the transpose of a numpy.ndarray."""
 
+
 def np_transpose(matrix):
     """Returns the transpose of a numpy.ndarray."""
     return matrix.T
