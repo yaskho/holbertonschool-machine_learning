59a8d8eb8c8837d12fd47b540fa1bc2e819e5dfa
yaskho <yassineholberton@gmail.com>
2025-12-08 13:06:41 +0100

    hgvg

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module for plotting a histogram of student grades."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plot a histogram of student scores with 10-unit bins."""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Histogram with bins every 10 units, outlined in black
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
+    # Labels
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+
+    # Title
+    plt.title("Project A")
+
+    # Display plot
+    plt.show()
