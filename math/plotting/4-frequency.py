bdefcdb5732d3d5130d82215f7ff6563828e2ee9
yaskho <yassineholberton@gmail.com>
2025-12-14 16:08:37 +0100

    hbbhv

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python3
-"""Module for plotting a histogram of student grades."""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Plot a histogram of student grades with 10-unit bins."""
+    """Plots a histogram of student grades."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    # Compute histogram counts
-    bins = np.arange(0, 110, 10)
-    counts, edges = np.histogram(student_grades, bins=bins)
+    plt.hist(student_grades, bins=range(0, 101, 10),
+             edgecolor='black')
 
-    # Centers of bins for bar alignment
-    centers = 0.5 * (edges[:-1] + edges[1:])
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
 
-    # Plot bars
-    plt.bar(centers, counts, width=10, edgecolor='black')
-
-    # Labels
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-
-    # Title
-    plt.title("Project A")
-
-    # Display
     plt.show()
