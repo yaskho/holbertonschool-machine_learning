1f7b1a3bfb17b4f954e7f40ddf84b093699f3293
yaskho <yassineholberton@gmail.com>
2025-12-14 16:15:41 +0100

    ffdf

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """Plots a stacked bar graph of fruit per person."""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    people = ['Farrah', 'Fred', 'Felicia']
+    x = np.arange(len(people))
+    width = 0.5
+
+    apples = fruit[0]
+    bananas = fruit[1]
+    oranges = fruit[2]
+    peaches = fruit[3]
+
+    plt.bar(x, apples, width, color='red', label='Apples')
+    plt.bar(x, bananas, width, bottom=apples,
+            color='yellow', label='Bananas')
+    plt.bar(x, oranges, width, bottom=apples + bananas,
+            color='#ff8000', label='Oranges')
+    plt.bar(x, peaches, width,
+            bottom=apples + bananas + oranges,
+            color='#ffe5b4', label='Peaches')
+
+    plt.xticks(x, people)
+    plt.ylabel('Quantity of Fruit')
+    plt.ylim(0, 80)
+    plt.yticks(np.arange(0, 81, 10))
+    plt.title('Number of Fruit per Person')
+    plt.legend()
+
+    plt.show()
