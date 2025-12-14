3ef69a4ea6f155e7c37d7b7b955e6c91a98897a8
yaskho <yassineholberton@gmail.com>
2025-12-14 16:10:31 +0100

    hghg

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -9,11 +9,13 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=range(0, 101, 10),
-             edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+
+    plt.xlim(0, 100)
+    plt.xticks(range(0, 101, 10))
 
     plt.show()
