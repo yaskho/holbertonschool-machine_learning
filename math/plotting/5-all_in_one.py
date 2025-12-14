bf983982203b87a28679ba17b4e2c8f0e958ea20
yaskho <yassineholberton@gmail.com>
2025-12-14 16:37:43 +0100

    hvvvgv

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def all_in_one():
-    """Plots all previous graphs in one figure."""
+    """Plots all 5 previous graphs in a single figure."""
     y0 = np.arange(0, 11) ** 3
 
     mean = [69, 0]
@@ -45,14 +45,14 @@ def all_in_one():
     ax1.set_xlabel('x', fontsize='x-small')
     ax1.set_ylabel('y', fontsize='x-small')
 
-    # Plot 3: Line plot (decay)
+    # Plot 3: Line plot (C-14 decay)
     ax2 = plt.subplot2grid((3, 2), (1, 0))
     ax2.plot(x2, y2)
     ax2.set_title('Exponential Decay of C-14', fontsize='x-small')
     ax2.set_xlabel('Time (years)', fontsize='x-small')
     ax2.set_ylabel('Fraction Remaining', fontsize='x-small')
 
-    # Plot 4: Multiple line plot
+    # Plot 4: Multiple decay lines
     ax3 = plt.subplot2grid((3, 2), (1, 1))
     ax3.plot(x3, y31, label='C-14')
     ax3.plot(x3, y32, label='Ra-226')
@@ -62,7 +62,7 @@ def all_in_one():
     ax3.set_ylabel('Fraction Remaining', fontsize='x-small')
     ax3.legend(fontsize='x-small')
 
-    # Plot 5: Histogram (spans two columns)
+    # Plot 5: Histogram (last row, spans two columns)
     ax4 = plt.subplot2grid((3, 2), (2, 0), colspan=2)
     ax4.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     ax4.set_title('Project A', fontsize='x-small')
@@ -70,5 +70,4 @@ def all_in_one():
     ax4.set_ylabel('Number of Students', fontsize='x-small')
     ax4.set_xlim(0, 100)
 
-    plt.tight_layout()
     plt.show()
