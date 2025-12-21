763434d3437ee96bf4b26975508bee5cf849a6da
yaskho <yassineholberton@gmail.com>
2025-12-21 20:50:55 +0100

    vdvfvf

---
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        return max(
+            self.left_child.max_depth_below(),
+            self.right_child.max_depth_below()
+        )
