aad889ec5b63f2c01566b992110fc397e4e6bd0a
yaskho <yassineholberton@gmail.com>
2026-04-26 11:14:59 +0100

    nhbhb

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-"""Neuron class"""
-
+"""Defines a single neuron for binary classification"""
 
 import numpy as np
 
 
 class Neuron:
-    """Defines a single neuron for binary classification"""
+    """Neuron class for binary classification"""
 
     def __init__(self, nx):
         """Initialize the neuron"""
@@ -19,8 +18,23 @@ class Neuron:
         self.__b = 0
         self.__A = 0
 
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
+
     def forward_prop(self, X):
-        """Calculates the forward propagation of the neuron"""
+        """Calculates forward propagation"""
         Z = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
