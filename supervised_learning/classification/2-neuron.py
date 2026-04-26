47efc9ba8bbb5fe7e84b174455f85f4b493459dd
yaskho <yassineholberton@gmail.com>
2026-04-26 11:04:19 +0100

    hbhb

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+This module defines a Neuron performing binary classification
+with private attributes and proper getters.
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Defines a single neuron performing binary classification.
+    """
+
+    def __init__(self, nx):
+        """
+        Class constructor for Neuron.
+
+        Args:
+            nx (int): The number of input features to the neuron.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the private weights vector __W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the private bias __b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the private activated output __A"""
+        return self.__A
