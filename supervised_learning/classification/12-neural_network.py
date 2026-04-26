2730edf00357d99f142a851210cc35207dca4360
yaskho <yassineholberton@gmail.com>
2026-04-26 18:39:40 +0100

    hbhb

---
+++ supervised_learning/classification/12-neural_network.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""Neural Network with one hidden layer"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Defines a neural network with one hidden layer"""
+
+    def __init__(self, nx, nodes):
+        """Initialize the neural network"""
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Performs forward propagation"""
+
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates logistic regression cost"""
+
+        m = Y.shape[1]
+        return - (1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        )
+
+    def evaluate(self, X, Y):
+        """Evaluates predictions"""
+
+        _, A2 = self.forward_prop(X)
+
+        prediction = np.where(A2 >= 0.5, 1, 0)
+        cost = self.cost(Y, A2)
+
+        return prediction, cost
