498c8106b0c16a5baa5505f031ef166549cae6d1
yaskho <yassineholberton@gmail.com>
2026-04-26 19:14:17 +0100

    hbhbb

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,40 @@
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network using Keras Sequential API.
+
+    Args:
+        nx (int): number of input features
+        layers (list): number of nodes in each layer
+        activations (list): activation functions for each layer
+        lambtha (float): L2 regularization parameter
+        keep_prob (float): probability a node is kept (dropout)
+
+    Returns:
+        keras.Model: compiled Keras sequential model
+    """
+    model = K.Sequential()
+
+    for i in range(len(layers)):
+        # L2 regularization
+        regularizer = K.regularizers.l2(lambtha)
+
+        # First layer needs input_dim
+        if i == 0:
+            model.add(K.layers.Dense(
+                units=layers[i],
+                activation=activations[i],
+                input_shape=(nx,),
+                kernel_regularizer=regularizer
+            ))
+        else:
+            model.add(K.layers.Dense(
+                units=layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer
+            ))
+
+        # Add dropout after every layer
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(rate=1 - keep_prob))
+
+    return model
