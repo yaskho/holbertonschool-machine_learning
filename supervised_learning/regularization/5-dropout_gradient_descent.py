6864f5aa74ec75e1050b475d262d346cacaed88c
yaskho <yassineholberton@gmail.com>
2026-04-29 22:14:19 +0100

    jnjnnj

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """updates weights with dropout"""
+    """updates weights using dropout gradient descent"""
     m = Y.shape[1]
     weights_copy = weights.copy()
 
@@ -17,17 +17,19 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         A_prev = cache["A" + str(l - 1)]
         W = weights_copy["W" + str(l)]
 
-        # Gradients
+        # Compute gradients
         dW = (dZ @ A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
-        # Update
+        # Update weights
         weights["W" + str(l)] -= alpha * dW
         weights["b" + str(l)] -= alpha * db
 
         if l > 1:
-            A_prev = cache["A" + str(l - 1)]
+            # Backprop using ORIGINAL weights
             dZ = W.T @ dZ
+
+            A_prev = cache["A" + str(l - 1)]
 
             # tanh derivative
             dZ = dZ * (1 - A_prev ** 2)
