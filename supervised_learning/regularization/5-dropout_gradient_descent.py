be94c8d26ff64d3b91800b61bdf980ffdb33a3b6
yaskho <yassineholberton@gmail.com>
2026-04-29 22:12:48 +0100

    hbhbb

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,46 +1,38 @@
 #!/usr/bin/env python3
-"""Module for gradient descent with dropout"""
+"""Dropout Gradient Descent"""
+
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """Updates weights using gradient descent with dropout
-
-    Args:
-        Y (numpy.ndarray): one-hot labels (classes, m)
-        weights (dict): weights and biases
-        cache (dict): activations and dropout masks
-        alpha (float): learning rate
-        keep_prob (float): probability to keep neuron
-        L (int): number of layers
-    """
+    """updates weights with dropout"""
     m = Y.shape[1]
     weights_copy = weights.copy()
 
-    # Last layer
-    dZ = cache['A' + str(L)] - Y
+    # Output layer
+    A_L = cache["A" + str(L)]
+    dZ = A_L - Y
 
-    for i in reversed(range(1, L + 1)):
-        A_prev = cache['A' + str(i - 1)]
-        W = weights_copy['W' + str(i)]
+    for l in reversed(range(1, L + 1)):
+        A_prev = cache["A" + str(l - 1)]
+        W = weights_copy["W" + str(l)]
 
-        dW = np.matmul(dZ, A_prev.T) / m
+        # Gradients
+        dW = (dZ @ A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
         # Update
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * db
+        weights["W" + str(l)] -= alpha * dW
+        weights["b" + str(l)] -= alpha * db
 
-        if i > 1:
-            dZ = np.matmul(W.T, dZ)
-            A_prev = cache['A' + str(i - 1)]
+        if l > 1:
+            A_prev = cache["A" + str(l - 1)]
+            dZ = W.T @ dZ
 
             # tanh derivative
             dZ = dZ * (1 - A_prev ** 2)
 
-            # Apply dropout mask
-            D = cache['D' + str(i - 1)]
+            # Apply dropout
+            D = cache["D" + str(l - 1)]
             dZ = dZ * D
-
-            # Scale
             dZ = dZ / keep_prob
