6fc51df14b43033733b47f60fbaec5973a62f279
yaskho <yassineholberton@gmail.com>
2026-05-10 20:53:27 +0100

    gcgv

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -10,21 +10,18 @@ class Exponential:
         Initialize the exponential distribution.
 
         Args:
-            data (list): List of data to estimate lambtha.
+            data (list): Data used to estimate lambtha.
             lambtha (float): Expected number of occurrences.
 
         Raises:
             TypeError: If data is not a list.
-            ValueError: If data contains fewer than 2 values.
+            ValueError: If data has fewer than 2 values.
             ValueError: If lambtha is not positive.
         """
 
         if data is None:
             if lambtha <= 0:
-                raise ValueError(
-                    "lambtha must be a positive value"
-                )
-
+                raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
 
         else:
@@ -32,9 +29,27 @@ class Exponential:
                 raise TypeError("data must be a list")
 
             if len(data) < 2:
-                raise ValueError(
-                    "data must contain multiple values"
-                )
+                raise ValueError("data must contain multiple values")
 
             mean = sum(data) / len(data)
             self.lambtha = 1 / float(mean)
+
+    def pdf(self, x):
+        """
+        Calculates the PDF for a given time period.
+
+        Args:
+            x (float): time period
+
+        Returns:
+            float: PDF value for x
+        """
+
+        x = float(x)
+
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+
+        return self.lambtha * (e ** (-self.lambtha * x))
