6905494e8f2ebff81e44be60f0f17ca932ea3884
yaskho <yassineholberton@gmail.com>
2026-05-31 22:29:23 +0100

    gcfcf

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
-EM = __import__('8-EM').expectation_maximization
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
@@ -16,17 +16,18 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None
 
     ks = list(range(kmin, kmax + 1))
+
     l_vals = []
     b_vals = []
     results = []
 
     for k in ks:
-        pi, m, S, g, l = EM(X, k, iterations, tol, verbose)
+        pi, m, S, g, l = expectation_maximization(X, k, iterations, tol, verbose)
 
         if pi is None:
             return None, None, None, None
 
-        # number of parameters
+        # number of parameters in GMM
         p = (k * d) + (k * d * (d + 1)) // 2 + (k - 1)
 
         bic = p * np.log(n) - 2 * l
