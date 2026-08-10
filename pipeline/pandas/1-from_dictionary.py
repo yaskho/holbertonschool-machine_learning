c3ee52822c7ed04d1841303a976918af28b989f4
yaskho <yassineholberton@gmail.com>
2026-08-10 18:05:39 +0100

    jbhbb

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Script that creates a pandas DataFrame from a dictionary.
+"""
+import pandas as pd
+
+data = {
+    'First': [0.0, 0.5, 1.0, 1.5],
+    'Second': ['one', 'two', 'three', 'four']
+}
+
+df = pd.DataFrame(data, index=['A', 'B', 'C', 'D'])
