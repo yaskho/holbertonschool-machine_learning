39c99ff265f6af71610552856d8c6af24091a77c
yaskho <yassineholberton@gmail.com>
2026-08-12 16:52:45 +0100

    hbhbbh

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python3
-"""Dropout Gradient Descent"""
+"""
+Module to update weights of a neural network with Dropout regularization
+using gradient descent.
+"""
 
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """updates weights using dropout gradient descent"""
+    """
+    Updates the weights of a neural network with Dropout regularization
+    using gradient descent.
+
+    Args:
+        Y: one-hot numpy.ndarray of shape (classes, m) containing
+           the correct labels for the data
+        weights: dictionary of the weights and biases of the neural network
+        cache: dictionary of the outputs and dropout masks of each layer
+        alpha: learning rate
+        keep_prob: probability that a node will be kept
+        L: number of layers of the network
+    """
     m = Y.shape[1]
-    weights_copy = weights.copy()
+    dz = cache['A' + str(L)] - Y
 
-    # Output layer
-    A_L = cache["A" + str(L)]
-    dZ = A_L - Y
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(i - 1)]
+        W_key = 'W' + str(i)
+        b_key = 'b' + str(i)
 
-    for l in reversed(range(1, L + 1)):
-        A_prev = cache["A" + str(l - 1)]
-        W = weights_copy["W" + str(l)]
+        W = weights[W_key]
 
-        # Compute gradients
-        dW = (dZ @ A_prev.T) / m
-        db = np.sum(dZ, axis=1, keepdims=True) / m
+        dW = (1 / m) * np.matmul(dz, A_prev.T)
+        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
 
-        # Update weights
-        weights["W" + str(l)] -= alpha * dW
-        weights["b" + str(l)] -= alpha * db
+        if i > 1:
+            dz = np.matmul(W.T, dz)
+            dz *= cache['D' + str(i - 1)]
+            dz /= keep_prob
+            dz *= (1 - (A_prev ** 2))
 
-        if l > 1:
-            # Backprop using ORIGINAL weights
-            dZ = W.T @ dZ
-
-            A_prev = cache["A" + str(l - 1)]
-
-            # tanh derivative
-            dZ = dZ * (1 - A_prev ** 2)
-
-            # Apply dropout
-            D = cache["D" + str(l - 1)]
-            dZ = dZ * D
-            dZ = dZ / keep_prob
+        weights[W_key] -= alpha * dW
+        weights[b_key] -= alpha * db
