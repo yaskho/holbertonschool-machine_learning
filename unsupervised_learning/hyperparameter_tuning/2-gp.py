e52f39cd5e74eae928da7fe8f953258dc328f48b
yaskho <yassineholberton@gmail.com>
2026-08-12 19:17:58 +0100

    jbkbjhb

--- unsupervised_learning/hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -1,77 +1,83 @@
-#!/usr/bin/env python3
-"""Gaussian Process module."""
+Here is the implementation for 2-gp.py with the update method added:
 
+Python
+#!/usr/bin/env python3
+"""
+Gaussian Process update module
+"""
 import numpy as np
 
 
 class GaussianProcess:
-    """Represents a noiseless 1D Gaussian Process."""
+    """
+    Represents a noiseless 1D Gaussian process
+    """
 
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
-        Initialize the Gaussian Process.
+        Class constructor for GaussianProcess
 
-        Args:
-            X_init: numpy.ndarray of shape (t, 1)
-            Y_init: numpy.ndarray of shape (t, 1)
-            l: length parameter of the RBF kernel
-            sigma_f: standard deviation of the output
+        Parameters:
+            X_init: numpy.ndarray of shape (t, 1) - sampled inputs
+            Y_init: numpy.ndarray of shape (t, 1) - sampled outputs
+            l: length scale parameter for the kernel
+            sigma_f: standard deviation multiplier for kernel output
         """
         self.X = X_init
         self.Y = Y_init
         self.l = l
         self.sigma_f = sigma_f
-        self.K = self.kernel(self.X, self.X)
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """
-        Compute the RBF covariance kernel matrix.
+        Calculates the covariance kernel matrix between two matrices
+        using the Radial Basis Function (RBF) kernel
 
-        Args:
+        Parameters:
             X1: numpy.ndarray of shape (m, 1)
             X2: numpy.ndarray of shape (n, 1)
 
         Returns:
-            numpy.ndarray of shape (m, n)
+            Covariance kernel matrix as a numpy.ndarray of shape (m, n)
         """
-        sqdist = (
-            np.sum(X1 ** 2, axis=1, keepdims=True)
-            + np.sum(X2 ** 2, axis=1)
-            - 2 * np.matmul(X1, X2.T)
-        )
-
-        return (self.sigma_f ** 2) * np.exp(
-            -sqdist / (2 * (self.l ** 2))
-        )
+        sqdist = (X1 - X2.T) ** 2
+        return (self.sigma_f ** 2) * np.exp(-0.5 * sqdist / (self.l ** 2))
 
     def predict(self, X_s):
         """
-        Predict the mean and variance at sample points.
+        Predicts the mean and variance of points in a Gaussian process
 
-        Args:
-            X_s: numpy.ndarray of shape (s, 1)
+        Parameters:
+            X_s: numpy.ndarray of shape (s, 1) containing all points
+                 whose mean and variance should be calculated
 
         Returns:
-            mu: numpy.ndarray of shape (s,)
-            sigma: numpy.ndarray of shape (s,)
+            mu: numpy.ndarray of shape (s,) containing the mean for
+                each point in X_s
+            sigma: numpy.ndarray of shape (s,) containing the variance for
+                   each point in X_s
         """
         K_s = self.kernel(self.X, X_s)
         K_ss = self.kernel(X_s, X_s)
         K_inv = np.linalg.inv(self.K)
 
-        mu = (K_s.T @ K_inv @ self.Y).reshape(-1)
-        sigma = np.diag(K_ss - K_s.T @ K_inv @ K_s)
+        mu = K_s.T @ K_inv @ self.Y
+        mu = mu.reshape(-1)
+
+        sigma_cov = K_ss - K_s.T @ K_inv @ K_s
+        sigma = np.diag(sigma_cov)
 
         return mu, sigma
 
     def update(self, X_new, Y_new):
         """
-        Update the Gaussian Process with a new sample.
+        Updates a Gaussian Process with a new sample point and value
 
-        Args:
-            X_new: numpy.ndarray of shape (1,)
-            Y_new: numpy.ndarray of shape (1,)
+        Parameters:
+            X_new: numpy.ndarray of shape (1,) - new sample point
+            Y_new: numpy.ndarray of shape (1,) - new sample function value
         """
-        self.X = np.vstack((self.X, X_new.reshape(1, 1)))
-        self.Y = np.vstack((self.Y, Y_new.reshape(1, 1)))
+        self.X = np.vstack((self.X, X_new.reshape(-1, 1)))
+        self.Y = np.vstack((self.Y, Y_new.reshape(-1, 1)))
         self.K = self.kernel(self.X, self.X)
