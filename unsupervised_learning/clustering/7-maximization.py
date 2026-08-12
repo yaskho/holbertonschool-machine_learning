d7e44a240951fa4fa9025c76b4d2e4c002ba3bc6
yaskho <yassineholberton@gmail.com>
2026-08-12 18:42:48 +0100

    hgchcjv

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,48 +1,45 @@
 #!/usr/bin/env python3
 """
-Maximization step for GMM (M-step)
+Contains the maximization function for a Gaussian Mixture Model
 """
-
 import numpy as np
 
 
 def maximization(X, g):
     """
-    Performs maximization step of EM algorithm.
+    Calculates the maximization step in the EM algorithm for a GMM
 
-    Parameters:
-        X (n, d): dataset
-        g (k, n): responsibilities
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        g: numpy.ndarray of shape (k, n) containing posterior probabilities
 
     Returns:
-        pi (k,), m (k, d), S (k, d, d)
+        pi, m, S, or None, None, None on failure
+        pi: numpy.ndarray of shape (k,) containing updated priors
+        m: numpy.ndarray of shape (k, d) containing updated centroid means
+        S: numpy.ndarray of shape (k, d, d) containing updated covariances
     """
-    try:
-        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-            return None, None, None
-        if not isinstance(g, np.ndarray) or len(g.shape) != 2:
-            return None, None, None
-
-        n, d = X.shape
-        k = g.shape[0]
-
-        Nk = np.sum(g, axis=1)  # (k,)
-
-        # priors
-        pi = Nk / n
-
-        m = np.zeros((k, d))
-        S = np.zeros((k, d, d))
-
-        for i in range(k):  # allowed single loop
-            # mean
-            m[i] = np.sum(g[i][:, None] * X, axis=0) / Nk[i]
-
-            # covariance
-            diff = X - m[i]
-            S[i] = (g[i][:, None] * diff).T @ diff / Nk[i]
-
-        return pi, m, S
-
-    except Exception:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+
+    n, d = X.shape
+    k, n_g = g.shape
+
+    if n != n_g:
+        return None, None, None
+    if not np.isclose(np.sum(g, axis=0), 1).all():
+        return None, None, None
+
+    Nk = np.sum(g, axis=1)
+    pi = Nk / n
+
+    m = np.matmul(g, X) / Nk[:, None]
+
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        diff = X - m[i]
+        S[i] = np.matmul(g[i] * diff.T, diff) / Nk[i]
+
+    return pi, m, S
