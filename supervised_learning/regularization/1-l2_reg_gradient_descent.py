c8617af54f5bc6ebab51c8db3c673ea01a0a595e
yaskho <yassineholberton@gmail.com>
2026-08-12 16:50:59 +0100

    jbhbjn

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python3
-"""Module for gradient descent with L2 regularization"""
+"""
+Module to update weights and biases using gradient descent
+with L2 regularization.
+"""
+
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """Updates weights using gradient descent with L2 regularization"""
+    """
+    Updates the weights and biases of a neural network using gradient
+    descent with L2 regularization.
+
+    Args:
+        Y: one-hot numpy.ndarray of shape (classes, m) containing
+           the correct labels for the data
+        weights: dictionary of the weights and biases of the neural network
+        cache: dictionary of the outputs of each layer of the neural network
+        alpha: learning rate
+        lambtha: L2 regularization parameter
+        L: number of layers of the network
+    """
     m = Y.shape[1]
-    weights_copy = weights.copy()
+    dz = cache['A' + str(L)] - Y
 
-    dZ = cache['A' + str(L)] - Y
-
-    for i in reversed(range(1, L + 1)):
+    for i in range(L, 0, -1):
         A_prev = cache['A' + str(i - 1)]
-        W = weights_copy['W' + str(i)]  # ← IMPORTANT
+        W_key = 'W' + str(i)
+        b_key = 'b' + str(i)
 
-        dW = (np.matmul(dZ, A_prev.T) / m) + (lambtha / m) * W
-        db = np.sum(dZ, axis=1, keepdims=True) / m
+        W = weights[W_key]
 
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * db
+        dW = (1 / m) * np.matmul(dz, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
 
         if i > 1:
-            dZ = np.matmul(W.T, dZ) * (1 - A_prev ** 2)
+            dz = np.matmul(W.T, dz) * (1 - (A_prev ** 2))
+
+        weights[W_key] -= alpha * dW
+        weights[b_key] -= alpha * db
