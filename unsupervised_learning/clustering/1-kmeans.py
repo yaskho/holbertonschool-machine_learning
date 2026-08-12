d0af7db5392baa424c7acbadb6c47861e4b13d9f
yaskho <yassineholberton@gmail.com>
2026-08-12 18:24:15 +0100

    gdhgb

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -3,6 +3,7 @@
 Contains the function kmeans that performs K-means clustering on a dataset
 """
 import numpy as np
+initialize = __import__('0-initialize').initialize
 
 
 def kmeans(X, k, iterations=1000):
@@ -28,32 +29,28 @@ def kmeans(X, k, iterations=1000):
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
+    C = initialize(X, k)
+    if C is None:
+        return None, None
+
     low = np.min(X, axis=0)
     high = np.max(X, axis=0)
 
-    # 1st call to np.random.uniform
-    C = np.random.uniform(low=low, high=high, size=(k, X.shape[1]))
-
-    # Loop 1: Iterations loop
-    for _ in range(iterations):
+    # Loop 1: Outer iterations loop
+    for i in range(iterations):
         C_prev = np.copy(C)
 
-        # Distance calculation
         distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=2)
         clss = np.argmin(distances, axis=1)
-
-        # 2nd call to np.random.uniform (covers potential empty clusters)
-        reinit = np.random.uniform(low=low, high=high, size=(k, X.shape[1]))
 
         # Loop 2: Cluster loop
         for j in range(k):
             points = X[clss == j]
             if len(points) == 0:
-                C[j] = reinit[j]
+                C[j] = np.random.uniform(low, high)
             else:
                 C[j] = np.mean(points, axis=0)
 
-        # Recalculate classes after centroid update
         distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=2)
         clss = np.argmin(distances, axis=1)
 
