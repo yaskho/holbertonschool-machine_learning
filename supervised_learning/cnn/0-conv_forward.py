2ee0945d067e3a2131c3163c60f85e9bfad88076
yaskho <yassineholberton@gmail.com>
2026-08-12 16:58:45 +0100

    hvgvhb

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    Performs forward propagation over a convolutional layer of a neural network.
+    Performs forward propagation over a convolutional layer.
 
     Args:
         A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
