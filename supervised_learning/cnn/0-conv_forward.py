36a83f2d173ff5bc2b3102369c7a0a5ed1c1ed78
yaskho <yassineholberton@gmail.com>
2026-08-12 16:57:47 +0100

    hubhbbj

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,67 +1,63 @@
 #!/usr/bin/env python3
-"""Convolutional forward propagation"""
+"""
+Module to perform forward propagation over a convolutional layer.
+"""
 
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    Performs forward propagation over a convolutional layer
+    Performs forward propagation over a convolutional layer of a neural network.
 
     Args:
-        A_prev: (m, h_prev, w_prev, c_prev)
-        W: (kh, kw, c_prev, c_new)
-        b: (1, 1, 1, c_new)
-        activation: activation function
-        padding: "same" or "valid"
-        stride: (sh, sw)
+        A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
+                the output of the previous layer
+        W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing
+           the kernels for the convolution
+        b: numpy.ndarray of shape (1, 1, 1, c_new) containing
+           the biases applied to the convolution
+        activation: activation function applied to the convolution
+        padding: string that is either "same" or "valid"
+        stride: tuple of (sh, sw) containing the strides for the convolution
 
     Returns:
-        Activated output of convolution layer
+        The output of the convolutional layer
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
 
-    # --- Padding ---
     if padding == "same":
         ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
         pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
-    else:
-        ph, pw = 0, 0
+    elif padding == "valid":
+        ph = 0
+        pw = 0
 
-    # Pad input
-    A_prev_pad = np.pad(
+    A_pad = np.pad(
         A_prev,
-        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
         mode='constant'
     )
 
-    # Output dimensions
-    h_out = (h_prev + 2 * ph - kh) // sh + 1
-    w_out = (w_prev + 2 * pw - kw) // sw + 1
+    h_out = int((h_prev + 2 * ph - kh) / sh) + 1
+    w_out = int((w_prev + 2 * pw - kw) / sw) + 1
 
-    # Initialize output
     Z = np.zeros((m, h_out, w_out, c_new))
 
-    # --- Convolution ---
     for i in range(h_out):
         for j in range(w_out):
-            row = i * sh
-            col = j * sw
+            for k in range(c_new):
+                v_start = i * sh
+                v_end = v_start + kh
+                h_start = j * sw
+                h_end = h_start + kw
 
-            region = A_prev_pad[:, row:row+kh, col:col+kw, :]  # (m, kh, kw, c_prev)
+                a_slice = A_pad[:, v_start:v_end, h_start:h_end, :]
+                kernel = W[:, :, :, k]
+                Z[:, i, j, k] = (
+                    np.sum(a_slice * kernel, axis=(1, 2, 3)) + b[0, 0, 0, k]
+                )
 
-            # Convolution over all filters
-            Z[:, i, j, :] = np.sum(
-                region[..., np.newaxis] * W,
-                axis=(1, 2, 3)
-            )
-
-    # Add bias (broadcast)
-    Z = Z + b
-
-    # Activation
-    A = activation(Z)
-
-    return A
+    return activation(Z)
